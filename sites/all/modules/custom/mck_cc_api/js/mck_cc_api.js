d491561b6f763d3fe1815f4b7b41a16b03671338
Faisal Shabir <faisal_shabir@external.mckinsey.com>
2020-01-06 17:54:08 -0500

    Mail + Blur Changes {Rajiv}

--- sites/all/modules/custom/mck_cc_api/js/mck_cc_api.js
+++ sites/all/modules/custom/mck_cc_api/js/mck_cc_api.js
@@ -121,7 +121,7 @@ function check_approved(){
                         }
                     });
                 $('#popup-izimodal-success').iziModal('open', this);
-                $()
+                $("body > *").removeClass("blur-all");
             } //end if toggle
 
         } //end attach
