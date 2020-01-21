8e210986a611cdae72e288fed47819da108fd272
Rajiv Ramroop <rajiv.ramroop@gmail.com>
2020-01-21 16:25:14 -0500

    fix js

--- sites/all/modules/custom/mck_cc_api/js/mck_cc_api.js
+++ sites/all/modules/custom/mck_cc_api/js/mck_cc_api.js
@@ -1,5 +1,5 @@
 // Read a page's GET URL variables and return them as an associative array. 
-alert("Test");
+
 function getUrlVars() {
     var vars = [], hash;
     var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
